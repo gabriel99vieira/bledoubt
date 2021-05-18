f691c9aa6ebeafaa5ac5d8d36e881c814f2ce5a4
Jimmy B <jeb482@cornell.edu>
2021-05-18 14:17:35 -0400

    Split up an SQL query for recent devices to be more readable

--- app/src/main/java/hawk/privacy/bledoubt/HistoryDao.java
+++ app/src/main/java/hawk/privacy/bledoubt/HistoryDao.java
@@ -35,7 +35,21 @@ public interface HistoryDao {
     @Query("SELECT * FROM DeviceMetadata WHERE is_safe")
     LiveData<List<DeviceMetadata>> loadSafeDeviceMetadataLive();
 
-    @Query("SELECT meta.* FROM DeviceMetadata AS meta LEFT JOIN BeaconDetection AS last_detection ON meta.bluetoothAddress = last_detection.bluetoothAddress WHERE last_detection.timestamp = (SELECT MAX(detection.timestamp) FROM BeaconDetection AS detection WHERE detection.bluetoothAddress = last_detection.bluetoothAddress) AND last_detection.timestamp > :start_time")
+    @Query(
+       "SELECT meta.* " +
+       "FROM " +
+       "DeviceMetadata AS meta " +
+       "LEFT JOIN " +
+       "BeaconDetection AS last_detection " +
+       "ON (meta.bluetoothAddress = last_detection.bluetoothAddress) " +
+       "WHERE last_detection.timestamp = ( " +
+       "  SELECT " +
+       "    MAX(detection.timestamp) " +
+       "  FROM " +
+       "    BeaconDetection AS detection " +
+       "  WHERE detection.bluetoothAddress = last_detection.bluetoothAddress " +
+       ") " +
+       "AND last_detection.timestamp > :start_time")
     LiveData<List<DeviceMetadata>> loadDeviceMetadataSinceTimeLive(String start_time);
 
     @Query("SELECT * FROM DeviceMetadata WHERE bluetoothAddress IN (:bluetoothAddresses)")
